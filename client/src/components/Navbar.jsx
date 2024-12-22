3905d96c925b89a7de992156b6cf82880c9a9f2e
Prathica Shetty M <123286880+PrathicaShettyM@users.noreply.github.com>
2024-12-22 19:09:06 +0530

    project initial setup

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <nav className="bg-cyan-950 p-4">
+      <div className="container mx-auto flex flex-wrap justify-between items-center">
+        <div className="text-white font-bold text-xl">VotersChain</div>
+
+        {/* hamburger icon */}
+        <div className="md:hidden"> {/* Hide tabs on breakpoints */}
+          <button onClick={toggleMobileMenu} className="text-white focus:outline-none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div
+          className={`w-full md:w-auto md:flex md:items-center ${
+            isMobileMenuOpen ? 'block' : 'hidden'
+          }`}
+        >
+          <div className="flex flex-col md:flex-row md:space-x-4 text-white w-full"> {/* Added w-full to the inner div */}
+            <Link to="/" className="block py-2 md:inline-block hover:text-gray-300">
+              Home
+            </Link>
+            <Link to="/aboutus" className="block py-2 md:inline-block hover:text-gray-300">
+              AboutUs
+            </Link>
+            <Link to="/results" className="block py-2 md:inline-block hover:text-gray-300">
+              Results
+            </Link>
+            <Link to="/candidates" className="block py-2 md:inline-block hover:text-gray-300">
+              Candidates
+            </Link>
+            <Link to="/elections" className="block py-2 md:inline-block hover:text-gray-300">
+              Elections
+            </Link>
+            <Link to="/profile" className="block py-2 md:inline-block hover:text-gray-300">
+              Profile
+            </Link>
+            <Link to="/login" className="block py-2 md:inline-block hover:text-gray-300">
+              Login
+            </Link>
+            <Link to="/logout" className="block py-2 md:inline-block hover:text-gray-300">
+              Logout
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import Navbar from '../components/Navbar'; // Import your Navbar
+import Footer from '../components/Footer'; // Import your Footer
+import heroImage from '../assets/heroImg.jpg'; // Import your image
+import {Link} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="h-[70vh] flex flex-col">
+      <Navbar /> {/* Navbar at the top */}
+
+      <main className="flex-grow flex items-center justify-center"> {/* Main content area */}
+        <div className="container mx-auto px-4 flex flex-col md:flex-row items-center h-full"> {/* Container for responsiveness */}
+          <div className="md:w-1/2 p-6 text-center md:text-left"> {/* Left side content */}
+            <h1 className="text-4xl font-bold mb-4 text-cyan-950">
+              Welcome to Voter Chain
+            </h1>
+            <p className="text-lg text-cyan-900 leading-relaxed mb-8">
+              Experience the future of secure and transparent elections with our
+              decentralized voting system. Built on blockchain technology, we
+              ensure verifiable, tamper-proof, and accessible voting for
+              everyone.
+            </p>
+            <Link to="/aboutus" className="bg-cyan-950 hover:border-2 hover:border-cyan-950 hover:bg-white hover:text-cyan-950 text-white font-bold py-3 px-10 rounded">
+              About Us
+            </Link>
+          </div>
+          <div className="md:w-1/2 p-6"> {/* Right side image */}
+            <img
+              src={heroImage}
+              alt="Decentralized Voting Illustration"
+              className="rounded-lg shadow-lg w-full h-auto object-cover"
+            />
+          </div>
+        </div>
+      </main>
+
+      <Footer /> 
+    </div>
+  );
+}
+
+export default Home;
